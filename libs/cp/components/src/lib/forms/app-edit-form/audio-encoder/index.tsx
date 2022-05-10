79071e49b54be5c5e0b3ba50b63f4eb0fb592fcb
Sergey Shevchenko <shevchenkosergeyalexandrovich@gmail.com>
2022-05-10 14:56:21 +0300

    upd audio-encoder component

--- libs/cp/components/src/lib/forms/app-edit-form/audio-encoder/index.tsx
+++ libs/cp/components/src/lib/forms/app-edit-form/audio-encoder/index.tsx
@@ -6,54 +6,52 @@ import "./audio-encoder.css";
 
 export const AudioEncoder: FC = () => {
     return (
-        <>
-            <BorderBox gap={24} className="audio-encoder-settings">
-                <Columns gap={24} col={3} className="audio-encoder-inputs">
-                    <Dropdown addClass="input-small" label="Audio Codec" />
-                    <Dropdown addClass="input-small" label="Bitrate" />
-                    <Dropdown addClass="input-small" label="SDI Pair" />
-                    <Dropdown addClass="input-small" label="AC3 Dialogue Level" />
-                    <Dropdown addClass="input-small" label="Channels" />
-                    <FlexHolder className="btn-block">
-                        <InputText className="input-small" label="Language" />
-                        <Button data-type="btn-icon">
-                            <Icon name="plus" />
-                        </Button>
-                    </FlexHolder>
-                </Columns>
-                <Columns gap={24} col={3}>
-                    <div className="text-holder">Codec1</div>
-                    <div className="text-holder">128 Mbps</div>
-                    <div className="text-holder">12</div>
-                    <div className="text-holder">max -31dB</div>
-                    <div className="text-holder">4</div>
-                    <FlexHolder className="edit-holder">
-                        <div className="text-holder">Spanish</div>
-                        <Button data-type="btn-icon">
-                            <Icon name="edit" />
-                        </Button>
-                        <Button data-type="btn-icon">
-                            <Icon name="trash" style={{color: "var(--danger)"}} />
-                        </Button>
-                    </FlexHolder>
-                </Columns>
-                <Columns gap={24} col={3}>
-                    <div className="text-holder">Codec1</div>
-                    <div className="text-holder">128 Mbps</div>
-                    <div className="text-holder">12</div>
-                    <div className="text-holder">max -31dB</div>
-                    <div className="text-holder">4</div>
-                    <FlexHolder className="edit-holder">
-                        <div className="text-holder">Spanish</div>
-                        <Button data-type="btn-icon">
-                            <Icon name="edit" />
-                        </Button>
-                        <Button data-type="btn-icon">
-                            <Icon name="trash" style={{color: "var(--danger)"}} />
-                        </Button>
-                    </FlexHolder>
-                </Columns>
-            </BorderBox>
-        </>
+        <BorderBox gap={24} className="audio-encoder-settings">
+            <Columns gap={24} col={3} className="audio-encoder-inputs">
+                <Dropdown addClass="input-small" label="Audio Codec" />
+                <Dropdown addClass="input-small" label="Bitrate" />
+                <Dropdown addClass="input-small" label="SDI Pair" />
+                <Dropdown addClass="input-small" label="AC3 Dialogue Level" />
+                <Dropdown addClass="input-small" label="Channels" />
+                <FlexHolder className="btn-block">
+                    <InputText className="input-small" label="Language" />
+                    <Button data-type="btn-icon">
+                        <Icon name="plus" />
+                    </Button>
+                </FlexHolder>
+            </Columns>
+            <Columns gap={24} col={3}>
+                <div className="text-holder">Codec1</div>
+                <div className="text-holder">128 Mbps</div>
+                <div className="text-holder">12</div>
+                <div className="text-holder">max -31dB</div>
+                <div className="text-holder">4</div>
+                <FlexHolder className="edit-holder">
+                    <div className="text-holder">Spanish</div>
+                    <Button data-type="btn-icon">
+                        <Icon name="edit" />
+                    </Button>
+                    <Button data-type="btn-icon">
+                        <Icon name="trash" style={{color: "var(--danger)"}} />
+                    </Button>
+                </FlexHolder>
+            </Columns>
+            <Columns gap={24} col={3}>
+                <div className="text-holder">Codec1</div>
+                <div className="text-holder">128 Mbps</div>
+                <div className="text-holder">12</div>
+                <div className="text-holder">max -31dB</div>
+                <div className="text-holder">4</div>
+                <FlexHolder className="edit-holder">
+                    <div className="text-holder">Spanish</div>
+                    <Button data-type="btn-icon">
+                        <Icon name="edit" />
+                    </Button>
+                    <Button data-type="btn-icon">
+                        <Icon name="trash" style={{color: "var(--danger)"}} />
+                    </Button>
+                </FlexHolder>
+            </Columns>
+        </BorderBox>
     );
 };
