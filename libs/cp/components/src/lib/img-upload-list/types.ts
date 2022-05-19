b2b7d57c9dd10e053ef23e3897bd1ac9f64f27d1
bender <sergeyh@nextologies.com>
2022-05-19 18:05:26 +0300

    libs/cp/components/src/lib/img-upload-list  - removed

--- libs/cp/components/src/lib/img-upload-list/types.ts
+++
@@ -1,9 +0,0 @@
-import {AnyAction} from "@reduxjs/toolkit";
-import {Dispatch} from "react";
-
-export type ImgUploadItemProps = {
-    dispatch: Dispatch<AnyAction>;
-    image?: string;
-    size?: string;
-    title?: string;
-};
