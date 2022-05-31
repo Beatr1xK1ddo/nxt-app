841891d2c536796d1d8c8d43597731f94bad6559
bender <sergeyh@nextologies.com>
2022-05-31 16:19:04 +0300

    libs/shared/components/src/lib/accordion  - refactoring libs/cp/components/src/lib/ipbe/list/item/card libs/cp/components/src/lib/ipbe/list/item/card/accordionHeader  - ipbe card PerformanceChart utilization  - refactoring

--- libs/cp/components/src/lib/ipbe/list/item/card/index.tsx
+++ libs/cp/components/src/lib/ipbe/list/item/card/index.tsx
@@ -6,13 +6,12 @@ import {Accordion, CheckboxComponent, Button, CircularProgressWithLabel, Tooltip
 import {EAppGeneralStatus, IIpbeListItem} from "@nxt-ui/cp/types";
 
 import {NodeStatus} from "../../../../common";
-import {CardAccordionTitle} from "./accordionTitle";
-
 import {FlexHolder} from "../../../../../index";
 
-import "./index.css";
+import IpbeCardAccordionHeader from "./accordionHeader";
+import PerformanceChart from "./performanceChart";
 
-import img from "../img.png";
+import "./index.css";
 
 interface IpbeCardItemProps {
     item: IIpbeListItem;
@@ -21,15 +20,7 @@ interface IpbeCardItemProps {
 }
 
 export const IpbeCardItem: FC<IpbeCardItemProps> = ({item, appStatus, startedAt}) => {
-    const {name, nodeText, ipbeDestinations, inputFormat, videoBitrate, cardIdx, ipbeAudioEncoders} = item;
-
-    const imageCss = useMemo(() => ({backgroundImage: `url(${img})`}), []);
-
-    /*
-     const thumbnail = useMemo(() => {
-     return <img style={{ width: '100%', aspectRatio: '16/9' }} src={thumbnail} />;
-     }, [thumbnail]);
-     */
+    const {name, inputFormat, videoBitrate, cardIdx, ipbeAudioEncoders} = item;
 
     const thumbnail = useMemo(() => {
         return <img style={{width: "100%", aspectRatio: "16/9"}} alt={"ipbe thumbnail"} />;
@@ -43,12 +34,6 @@ export const IpbeCardItem: FC<IpbeCardItemProps> = ({item, appStatus, startedAt}
         }
     }, [appStatus, startedAt]);
 
-    const performanceChart = useMemo(() => {
-        return (
-            <div style={{width: "100%", aspectRatio: "16/9", backgroundColor: "cyan"}}>Performance chart goes here</div>
-        );
-    }, []);
-
     return (
         <li className="card-wrap">
             <section className="card-holder">
@@ -59,85 +44,69 @@ export const IpbeCardItem: FC<IpbeCardItemProps> = ({item, appStatus, startedAt}
                     <h4 className="card-title">
                         <Icon name="allocation" /> {name}
                     </h4>
-                    <Accordion
-                        header={<CardAccordionTitle title={"Info"} paragraph={""} />}
-                        content={
-                            <div className="info-block">
-                                <TooltipComponent
-                                    className="white-tooltip"
-                                    arrow={true}
-                                    title={
-                                        <>
-                                            <p className="heading">NXT-RXm3-4S-359</p>
-                                            <dl>
-                                                <dt>Code:</dt>
-                                                <dd>M963245</dd>
-                                            </dl>
-                                            <p>
-                                                <a href="/">central login ssh nxta@localhost -p 48241</a>
-                                            </p>
-                                            <a href="/">Applications dashboard</a>
-                                        </>
-                                    }>
-                                    <p className="card-text">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit?
-                                    </p>
-                                </TooltipComponent>
-                                <ul className="card-table-list">
-                                    <li>
-                                        <p className="text-small">{runTime}</p>
-                                    </li>
-                                    <li>
-                                        <span className="text-thin">IDX:</span>
-                                        <p className="text-small">{cardIdx}</p>
-                                    </li>
-                                    <li>
-                                        <span className="text-thin">Format:</span>
-                                        <p className="text-small">{inputFormat}</p>
-                                    </li>
-                                    <li>
-                                        <div className="bitrate-holder">
-                                            {videoBitrate && <p className="text-small">{`${videoBitrate}Mbps`}</p>}
-                                            {ipbeAudioEncoders?.map((item) => (
-                                                <p className="text-small">{`${item.bitrate}kbps ${item.codec}`}</p>
-                                            ))}
-                                        </div>
-                                    </li>
-                                </ul>
-                                <FlexHolder justify="flex-start" className="card-info">
-                                    <CircularProgressWithLabel value={80} />
-                                    <NodeStatus status={appStatus} />
-                                    <Button data-type="btn-icon">
-                                        <Icon name="calendar" />
-                                    </Button>
-                                </FlexHolder>
-                            </div>
-                        }
-                    />
-                    <Accordion
-                        header={
-                            <CardAccordionTitle
-                                title={"Performance chart"}
-                                paragraph={
+                    <Accordion header={<IpbeCardAccordionHeader title={"Info"} paragraph={""} />}>
+                        <div className="info-block">
+                            <TooltipComponent
+                                className="white-tooltip"
+                                arrow={true}
+                                title={
                                     <>
-                                        239.5.171.8:1234 - 2Mbps <strong>- 6.7 Mbps</strong>
-                                        <br />
-                                        239.5.171.8:1234 - 2Mbps <strong className="mark">- 6.7 Mbps</strong>
+                                        <p className="heading">NXT-RXm3-4S-359</p>
+                                        <dl>
+                                            <dt>Code:</dt>
+                                            <dd>M963245</dd>
+                                        </dl>
+                                        <p>
+                                            <a href="/">central login ssh nxta@localhost -p 48241</a>
+                                        </p>
+                                        <a href="/">Applications dashboard</a>
                                     </>
-                                }
-                            />
-                        }
-                        content={performanceChart}
-                    />
+                                }>
+                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit?</p>
+                            </TooltipComponent>
+                            <ul className="card-table-list">
+                                <li>
+                                    <p className="text-small">{runTime}</p>
+                                </li>
+                                <li>
+                                    <span className="text-thin">IDX:</span>
+                                    <p className="text-small">{cardIdx}</p>
+                                </li>
+                                <li>
+                                    <span className="text-thin">Format:</span>
+                                    <p className="text-small">{inputFormat}</p>
+                                </li>
+                                <li>
+                                    <div className="bitrate-holder">
+                                        {videoBitrate && <p className="text-small">{`${videoBitrate}Mbps`}</p>}
+                                        {ipbeAudioEncoders?.map((item) => (
+                                            <p className="text-small">{`${item.bitrate}kbps ${item.codec}`}</p>
+                                        ))}
+                                    </div>
+                                </li>
+                            </ul>
+                            <FlexHolder justify="flex-start" className="card-info">
+                                <CircularProgressWithLabel value={80} />
+                                <NodeStatus status={appStatus} />
+                                <Button data-type="btn-icon">
+                                    <Icon name="calendar" />
+                                </Button>
+                            </FlexHolder>
+                        </div>
+                    </Accordion>
+                    {item.monitoring &&
+                        item.ipbeDestinations.map((destination) => (
+                            <PerformanceChart nodeId={item.node} destination={destination} />
+                        ))}
                     <Accordion
                         header={
-                            <CardAccordionTitle
+                            <IpbeCardAccordionHeader
                                 title={"Media view"}
                                 paragraph={format(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSSxxx")}
                             />
-                        }
-                        content={thumbnail}
-                    />
+                        }>
+                        {thumbnail}
+                    </Accordion>
                 </div>
             </section>
             <ul className="card-icon-list">
--- libs/shared/components/src/lib/accordion/types.ts
+++ libs/shared/components/src/lib/accordion/types.ts
@@ -1,8 +1,6 @@
 import {ReactChild, ReactNode} from "react";
 import {AccordionProps} from "@mui/material/Accordion";
 
-export type IAccordionComponentProps = {
+export interface IAccordionComponentProps extends AccordionProps {
     header: ReactChild | ReactNode;
-    content: ReactChild | ReactNode;
-    style?: AccordionProps["sx"];
-};
+}
