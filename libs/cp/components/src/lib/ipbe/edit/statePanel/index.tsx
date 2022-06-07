d8f034ecf699caed8f191004094a520d69342c4f
bender <sergeyh@nextologies.com>
2022-06-07 18:10:16 +0300

    libs/cp/components/src/lib/ipbe/edit/statePanel/destinations libs/cp/components/src/lib/ipbe/edit/statePanel/destinations/destination  - introduced as separate component  - refactoring libs/cp/components/src/lib/ipbe/edit/statePanel/nodeSystemState  - initial impl libs/cp/components/src/lib/ipbe/edit/statePanel  - utilize NodeSystemState and Destinations  - refactoring/cleanup

--- libs/cp/components/src/lib/ipbe/edit/statePanel/index.tsx
+++ libs/cp/components/src/lib/ipbe/edit/statePanel/index.tsx
@@ -1,18 +1,16 @@
-import {MouseEvent, SyntheticEvent, useMemo, useState} from "react";
+import {SyntheticEvent, useState} from "react";
 
 import {Button, CircularProgressWithLabel, MenuComponent, MenuItemStyled} from "@nxt-ui/components";
 import {Icon} from "@nxt-ui/icons";
 
-import {FlexHolder, GridRow, LogContainer, TabElement, TabHolder, TabPanel} from "../../../common";
+import {FlexHolder, LogContainer, TabElement, TabHolder, TabPanel} from "../../../common";
 
 import img01 from "./assets/img01-small.png";
 
+import NodeSystemState from "./nodeSystemState";
+import Destinations from "./destinations";
+
 import "./index.css";
-import {useSelector} from "react-redux";
-import {ipbeEditSelectors} from "@nxt-ui/cp-redux";
-import Destination from "./destination";
-import {useRealtimeNodeData} from "@nxt-ui/cp/hooks";
-import {memoryFormatter} from "@nxt-ui/cp/utils";
 
 const postsLog = [
     {
@@ -101,9 +99,6 @@ const menuLog = [
 ];
 
 export function StatePanel() {
-    const {node: nodeId, ipbeDestinations} = useSelector(ipbeEditSelectors.selectMainValues);
-    const {systemState, lastPing} = useRealtimeNodeData(nodeId);
-
     const [value, setValue] = useState(0);
     const tabChange = (event: SyntheticEvent, newValue: number) => {
         setValue(newValue);
@@ -120,41 +115,9 @@ export function StatePanel() {
 
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
-    const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
     const handleClose = () => {
         setAnchorEl(null);
     };
-
-    const destinations = useMemo(() => {
-        if (nodeId && ipbeDestinations) {
-            return ipbeDestinations.map((destination) => <Destination nodeId={nodeId} destination={destination} />);
-        } else {
-            return null;
-        }
-    }, [nodeId, ipbeDestinations]);
-
-    const nodeSystemSate = useMemo(() => {
-        return (
-            <>
-                <div>
-                    <span>Cpu/Governor mode</span>
-                    <strong>{`${systemState.cpu}% (powersave ??)`}</strong>
-                </div>
-                <div>
-                    <span>Load Average</span>
-                    <strong>{`${systemState.loadAverage} (CPU cores: ??)`}</strong>
-                </div>
-                <div>
-                    <span>Memory</span>
-                    <strong>
-                        {`${memoryFormatter(systemState.memoryUsed)}/${memoryFormatter(systemState.memoryTotal)}`}
-                    </strong>
-                </div>
-            </>
-        );
-    }, [systemState]);
 
     return (
         <section className="app-log">
@@ -186,8 +149,12 @@ export function StatePanel() {
                 </MenuComponent>
             </FlexHolder>
 
-            <div className="bitrate-log-holder">{destinations}</div>
-            <div className="node-system-sate">{nodeSystemSate}</div>
+            <div className="bitrate-log-holder">
+                <Destinations />
+            </div>
+            <div className="node-system-sate">
+                <NodeSystemState />
+            </div>
 
             <TabHolder value={value} onChange={tabChange} aria-label="tabs">
                 {tabs.map((item) => (
