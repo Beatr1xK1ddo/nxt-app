5efbca8b3a478646213c0744161b5028bc763628
Sergey Shevchenko <shevchenkosergeyalexandrovich@gmail.com>
2022-07-01 11:49:28 +0300

    upd progressbar component
    /nxt-ui/libs/shared/components/src/lib/progressbar/index.tsx
    - update styles and structure for progressbat component

--- libs/shared/icons/src/lib/icon-list.ts
+++ libs/shared/icons/src/lib/icon-list.ts
@@ -43,3 +43,4 @@ export {ReactComponent as allocation} from "../assets/allocation.svg";
 export {ReactComponent as port} from "../assets/port.svg";
 export {ReactComponent as port1} from "../assets/port1.svg";
 export {ReactComponent as settings} from "../assets/settings.svg";
+export {ReactComponent as qos} from "../assets/qos.svg";
