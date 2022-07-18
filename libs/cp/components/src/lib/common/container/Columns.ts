520c370fc452b08d893ef5fbc144a03cd4ba3b2b
Sergey Shevchenko <shevchenkosergeyalexandrovich@gmail.com>
2022-07-18 16:11:34 +0400

    fix txr-form adaptive
    \libs\cp\components\src\lib\txr\edit\form\main\
    - add adaptive fix for form columns grid
    
    \libs\cp\components\src\lib\common\container\
    - add alternative variant of tw-column grid

--- libs/cp/components/src/lib/common/container/Columns.ts
+++ libs/cp/components/src/lib/common/container/Columns.ts
@@ -1,15 +1,25 @@
 import {FC} from "react";
 import styled from "@emotion/styled";
 
-export const Columns: FC<{gap?: number; valign?: string; col?: number; className?: string}> = styled("div")<{
+export const Columns: FC<{gap?: number; valign?: string; col?: number | string; className?: string}> = styled("div")<{
     gap?: number;
-    col?: number;
+    col?: number | string;
     valign?: string;
 }>(
     ({gap, col}) => `
     gap: ${gap ? gap : 24}px;
     display: grid;
-    grid-template-columns: ${col === 2 ? "1fr 1fr" : col === 3 ? "1fr 1fr 1fr" : col === 4 ? "1fr 1fr 1fr 1fr" : "1fr"};
+    grid-template-columns: ${
+        col === 2
+            ? "1fr 1fr"
+            : col === "2minmax"
+            ? "minmax(60px, 1fr) minmax(60px, 1fr)"
+            : col === 3
+            ? "1fr 1fr 1fr"
+            : col === 4
+            ? "1fr 1fr 1fr 1fr"
+            : "1fr"
+    };
     grid-auto-flow: row;
     margin:0 0 ${gap ? gap : 24}px;
     align-items: self-start;
