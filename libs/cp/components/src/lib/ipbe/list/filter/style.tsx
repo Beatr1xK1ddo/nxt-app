a38e001c4dbb62ec217375b86aa6caa821e962e7
Sergey Shevchenko <shevchenkosergeyalexandrovich@gmail.com>
2022-09-26 13:34:28 +0200

    fix filterList adaptive
    libs\cp\components\src\lib\ipbe\list\filter\
    - update styles and structure for filter list

--- libs/cp/components/src/lib/ipbe/list/filter/style.tsx
+++ libs/cp/components/src/lib/ipbe/list/filter/style.tsx
@@ -2,24 +2,19 @@ import styled from "@emotion/styled";
 
 export const FilterList = styled("div")`
     width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: space-between;
     padding: 4px 16px 8px;
-    > div:not(.filter-buttons) {
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    gap: 0 10px;
+
+    > div{
         margin: 0 0 24px;
-        width: calc(100% / 6 - 10px);
     }
     @media (max-width: 992px) {
-        > div:not(.filter-buttons) {
-            width: calc(100% / 5 - 10px);
-        }
-        @media (max-width: 768px) {
-            > div:not(.filter-buttons) {
-                width: calc(100% / 3 - 10px);
-            }
-        }
+        grid-template-columns: repeat(5, 1fr);
+    }
+    @media (max-width: 768px) {
+        grid-template-columns: repeat(3, 1fr);
     }
 `;
 
