60a28e82bc4d0c9675b34b858206959511572e02
Sergey Shevchenko <shevchenkosergeyalexandrovich@gmail.com>
2022-09-29 17:01:29 +0200

    update gridRow somponent
    libs\cp\components\src\lib\common\gridRow\index.tsx
    - move all styles from .css-file to component with styled

--- libs/cp/components/src/lib/common/gridRow/index.tsx
+++ libs/cp/components/src/lib/common/gridRow/index.tsx
@@ -1,8 +1,56 @@
 import {FC} from "react";
 
 import {IPost} from "@nxt-ui/cp/types";
+import clsx from "clsx";
+import styled from "@emotion/styled";
 
-import "./index.css";
+const GridRowList = styled.ul`
+    display: grid;
+    gap: 2px 15px;
+    font-size: calc(var(--fz) - 4px);
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: auto 1fr;
+    grid-auto-columns: 1fr 1fr 1fr;
+    grid-auto-rows: 1fr 1fr;
+    grid-auto-flow: column;
+    font-weight: 600;
+    margin: 0 0 12px;
+    text-align: left;
+    .app-log & {
+        padding: 0 16% 0 0;
+    }
+    @media (max-width: 992px) {
+        .app-log & {
+            /*--q-l*/
+            padding: 0;
+            text-align: center;
+        }
+    }
+    .speed-ok {
+        color: var(--ok);
+    }
+    .speed-bad {
+        color: var(--r-premium);
+    }
+    .img-graph {
+        display: block;
+        margin: 3px 0 0;
+    }
+    & > li[class^="speed"] img {
+        display: block;
+    }
+    .text-light {
+        font-weight: 300;
+    }
+    .text-bold {
+        font-weight: 700;
+        color: var(--blacked);
+    }
+    .text-c {
+        display: block;
+        text-align: center;
+    }
+`;
 
 interface IGridRowProps {
     posts: IPost[];
@@ -11,10 +59,10 @@ interface IGridRowProps {
 
 export const GridRow: FC<IGridRowProps> = ({posts, className}) => {
     return (
-        <ul className={className ? `${className} grid-column` : "grid-column"}>
+        <GridRowList className={clsx(className && className)}>
             {posts.map((post) => (
                 <li key={post.id}>{post.content}</li>
             ))}
-        </ul>
+        </GridRowList>
     );
 };
