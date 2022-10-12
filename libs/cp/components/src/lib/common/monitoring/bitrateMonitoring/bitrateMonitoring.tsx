adddef4db3aced88263d02e0d0a6d14b9a57e32e
Kate <ekaterinas@nextologies.com>
2022-10-12 16:48:46 +0300

    libs/cp/components/src/lib/common/monitoring/bitrateMonitoring/bitrateMonitoring.tsx libs/ts-monitoring/components/src/lib/index.tsx - add draft graph

--- libs/cp/components/src/lib/common/monitoring/bitrateMonitoring/bitrateMonitoring.tsx
+++ libs/cp/components/src/lib/common/monitoring/bitrateMonitoring/bitrateMonitoring.tsx
@@ -14,7 +14,7 @@ import {commonSelectors} from "@nxt-ui/cp-redux";
 
 const DURATION_TIME = 1000;
 
-const plotOptions = {
+const defaultOptions = (options?: any) => ({
     marks: [
         bitrateArea(
             {
@@ -77,18 +77,18 @@ const plotOptions = {
     marginRight: 10,
     marginTop: 10,
     marginBottom: 20,
-    width: 280,
-    height: 198,
+    width: options.size.width || 280,
+    height: options.size.height || 198,
     style: {
         background: "none",
     },
-};
+});
 
-const BitrateMonitoringPlot = ({data}: any) => {
+const BitrateMonitoringPlot = ({data, options}: any) => {
     const ref = useRef();
     const refMoment = useRef();
     const visible = useSelector(commonSelectors.baseApp.selectTabVisible);
-    const barChart = useRef(Plot.plot(plotOptions));
+    const barChart = useRef(Plot.plot(defaultOptions(options)));
 
     useEffect(() => {
         if (data) {
--- libs/ts-monitoring/components/src/lib/index.tsx
+++ libs/ts-monitoring/components/src/lib/index.tsx
@@ -1,7 +1,8 @@
 import {MonitoringTable} from "./tsTable";
 import {TsMonitoringTree} from "./tsTree";
 import styled from "@emotion/styled";
-import {useRealtimeTsMonitoring} from "@nxt-ui/cp/hooks";
+import {useRealtimeMonitoring, useRealtimeTsMonitoring} from "@nxt-ui/cp/hooks";
+import {BitrateMonitoring} from "@nxt-ui/cp/components";
 
 const TsMonitoringContainer = styled.div`
     display: flex;
@@ -15,12 +16,18 @@ export const TsMonitoring = () => {
     const port = 1234;
 
     const {programs, p1Errors, p2Errors} = useRealtimeTsMonitoring(nodeId, ip, port);
+    const {monitoring: monitoringData} = useRealtimeMonitoring(nodeId, ip, port);
 
     return (
-        <TsMonitoringContainer>
-            <TsMonitoringTree programs={programs} />
-            <MonitoringTable header="PRIORITY 1" values={p1Errors} />
-            <MonitoringTable header="PRIORITY 2" values={p2Errors} />
-        </TsMonitoringContainer>
+        <>
+            <TsMonitoringContainer>
+                <TsMonitoringTree programs={programs} />
+                <MonitoringTable header="PRIORITY 1" values={p1Errors} />
+                <MonitoringTable header="PRIORITY 2" values={p2Errors} />
+            </TsMonitoringContainer>
+
+            {/* @ts-ignore */}
+            {monitoringData && <BitrateMonitoring data={monitoringData} options={{size: {width: 600, height: 450}}} />}
+        </>
     );
 };
