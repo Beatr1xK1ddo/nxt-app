2a712f33ea57e830c45cc24456479fafd6de4bc4
Sergey Shevchenko <shevchenkosergeyalexandrovich@gmail.com>
2022-12-07 18:57:33 +0100

    more adaptive styles

--- libs/shared/components/src/lib/breacrumbs/index.tsx
+++ libs/shared/components/src/lib/breacrumbs/index.tsx
@@ -3,20 +3,20 @@ import MUIBreadcrumbs, {BreadcrumbsProps} from "@mui/material/Breadcrumbs";
 import {FC} from "react";
 
 export const Breadcrumbs: FC<BreadcrumbsProps> = styled(MUIBreadcrumbs)`
-    margin: 0 0 8px;
+    margin: 0 0 0.5rem;
     .MuiBreadcrumbs-li {
         display: inline-block;
         vertical-align: middle;
-        font-size: calc(var(--fz) - 1px);
-        line-height: calc(var(--fz) + 4px);
-        margin: 0 0 8px;
+        font-size: calc(var(--fz) - 0.0625rem);
+        line-height: calc(var(--fz) + 0.25rem);
+        margin: 0 0 0.5rem;
         a {
             color: var(--grey-black);
             display: inline-block;
             vertical-align: middle;
             background: var(--pale-str);
-            padding: 5px 8px;
-            border-radius: 14px;
+            padding: 0.3125rem 0.5rem;
+            border-radius: 0.875rem;
             box-sizing: border-box;
             text-decoration: none;
             &:hover {
@@ -25,11 +25,11 @@ export const Breadcrumbs: FC<BreadcrumbsProps> = styled(MUIBreadcrumbs)`
             }
         }
         p {
-            padding: 5px 8px;
+            padding: 0.3125rem 0.5rem;
             margin: 0;
         }
     }
     .MuiBreadcrumbs-separator {
-        margin-bottom: 8px;
+        margin-bottom: 0.5rem;
     }
 `;
