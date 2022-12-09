ebc5711406d9264871c6dba55f55790c2a329c48
Kan Alexandr <alexander.kan@mirapolis.ru>
2022-12-09 16:38:23 +0300

    libs/cp/components/src/lib/common/notification/notificationBox - Fix component logic libs/cp/hooks/src/lib/index.tsx - Fix user notification hook logic libs/shared/components/src/lib/datepicker/datePickerInput/index.tsx - Fix component logic

--- libs/cp/components/src/lib/common/notification/notificationBox/dateRange/index.tsx
+++ libs/cp/components/src/lib/common/notification/notificationBox/dateRange/index.tsx
@@ -1,4 +1,4 @@
-import {ChangeEventHandler, FC} from "react";
+import {ChangeEventHandler, FC, useState} from "react";
 import {Button, DatePickerInput, InputText} from "@nxt-ui/components";
 import {FlexHolder} from "../../../container";
 
@@ -6,8 +6,8 @@ type INotificationBoxDateRangeProps = {
     search?: string;
     fromDate?: Date;
     toDate?: Date;
-    onFromChange?(value: Date): void;
-    onToChange?(value: Date): void;
+    onFromChange(value: any): void;
+    onToChange(value: any): void;
     onSearch?: ChangeEventHandler<HTMLInputElement>;
 };
 
@@ -19,12 +19,26 @@ export const NotificationBoxDateRange: FC<INotificationBoxDateRangeProps> = ({
     onToChange,
     onSearch,
 }) => {
+    const [fromOpen, setFromOpen] = useState(false);
+    const [toOpen, setToOpen] = useState(false);
+
+    const onFromChangeHandler = (value: any) => {
+        setFromOpen(false);
+        onFromChange(value);
+    };
+    const onToChangeHandler = (value: any) => {
+        setToOpen(false);
+        onToChange(value);
+    };
     return (
         <div className="range-box">
             <InputText label="Search" value={search} fullWidth onChange={onSearch} />
             <strong>Date range</strong>
             <FlexHolder>
                 <DatePickerInput
+                    onOpen={() => setFromOpen(true)}
+                    onClose={() => setFromOpen(false)}
+                    open={fromOpen}
                     className="testClass"
                     PopperProps={{
                         placement: "bottom-start",
@@ -32,32 +46,47 @@ export const NotificationBoxDateRange: FC<INotificationBoxDateRangeProps> = ({
                     }}
                     showToolbar={true}
                     label="From"
-                    value={fromDate}
-                    onChange={onFromChange}
+                    value={fromDate || null}
+                    onChange={onFromChangeHandler}
                     ToolbarComponent={(props: any) => (
                         <FlexHolder className="datepicker-toolbar">
+                            <Button
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    onFromChangeHandler(undefined);
+                                }}>
+                                Clear
+                            </Button>
                             <Button>Morning</Button>
                             <Button>Day</Button>
                             <Button>Week</Button>
-                            <Button>Night</Button>
                         </FlexHolder>
                     )}
                 />
                 <DatePickerInput
+                    open={toOpen}
+                    onOpen={() => setToOpen(true)}
+                    onClose={() => setToOpen(false)}
                     PopperProps={{
                         placement: "bottom-start",
                         disablePortal: true,
                     }}
                     showToolbar={true}
                     label="To"
-                    value={toDate}
-                    onChange={onToChange}
+                    value={toDate || null}
+                    onChange={onToChangeHandler}
                     ToolbarComponent={(props: any) => (
                         <FlexHolder className="datepicker-toolbar">
+                            <Button
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    onToChangeHandler(undefined);
+                                }}>
+                                Clear
+                            </Button>
                             <Button>Morning</Button>
                             <Button>Day</Button>
                             <Button>Week</Button>
-                            <Button>Night</Button>
                         </FlexHolder>
                     )}
                 />
