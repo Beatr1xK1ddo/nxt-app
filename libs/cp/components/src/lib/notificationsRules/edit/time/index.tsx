28c78cbe0dabf182bb1f6695b18ce3d549de5c0f
Sergey Shevchenko <shevchenkosergeyalexandrovich@gmail.com>
2022-12-09 14:46:18 +0100

    more updates for notifications
    libs\cp\components\src\lib\notificationsRules\edit\style.ts
    - uodate tab butons padding
    
    libs\cp\components\src\lib\notificationsRules\edit\time\index.tsx
    - delete extra array

--- libs/cp/components/src/lib/notificationsRules/edit/time/index.tsx
+++ libs/cp/components/src/lib/notificationsRules/edit/time/index.tsx
@@ -33,16 +33,6 @@ export const NotificationRuleTime: FC = () => {
         },
         [dispatch]
     );
-
-    const radioDay = [
-        {id: 1, value: "monday", label: "Mo"},
-        {id: 2, value: "tuesday", label: "Tu"},
-        {id: 3, value: "wednesday", label: "We"},
-        {id: 4, value: "thursday", label: "Th"},
-        {id: 5, value: "friday", label: "Fr"},
-        {id: 6, value: "saturday", label: "Sa"},
-        {id: 7, value: "sunsday", label: "Su"},
-    ];
     return (
         <Accordion className="accordion-ui" active header={"DAY AND TIME RANGE"} defaultExpanded>
             <FlexHolder justify="flex-start" className="time-picker-holder">
