cbacc85758b5b0beb22b45cf8adaaf1aff72d928
Kate <ekaterinas@nextologies.com>
2022-12-21 22:08:46 +0530

    ibs/cp/api/src/lib/axios.ts - fix baseUrl

--- libs/cp/api/src/lib/axios.ts
+++ libs/cp/api/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let headers: {[header: string]: string} | undefined = undefined;
-let baseURL = "https://qa.nextologies.com/";
+let baseURL = "/";
 
 if (process.env["NODE_ENV"] === "development") {
     baseURL = "https://dev-api-qa.nextologies.com/";
